eae76cdac9d276f4af984d18903a85efa78277d9
Quốc Cường <150774639+cuongdola0@users.noreply.github.com>
2025-05-01 13:58:29 +0700

    DiceSum

--- DiceSum.java
+++ DiceSum.java
@@ -1,38 +1,42 @@
-/*
-JDice: Java Dice Rolling Program
-Copyright (C) 2006 Andrew D. Hilton  (adhilton@cis.upenn.edu)
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
- */
-
+// Lớp DiceSum kế thừa từ DieRoll, dùng để cộng kết quả của 2 lần tung xúc xắc
 public class DiceSum extends DieRoll {
-    private DieRoll r1
-    private DieRoll r2;
-    public DiceSum(DieRoll r1
-		   DieRoll r2) {
-	super(0,0,0);
-	this.r1=r1;
-	this.r2=r2;
-    }
-//    public RollResult makeRoll() {
-	return r1makeRoll().andThen(r2makeRoll());
-    }
-    public String toString() {
-	return r1toString() + " & "+r2.toString();
+    private static final Logger logger = Logger.getLogger(DiceSum.class.getName()); // Logger cho lớp DiceSum
+
+    private DieRoll r1; // Biến thành viên đại diện cho lần tung xúc xắc thứ nhất
+    private DieRoll r2; // Biến thành viên đại diện cho lần tung xúc xắc thứ hai
+
+    // Constructor nhận vào 2 đối tượng DieRoll, khởi tạo lớp cha với giá trị mặc định
+    public DiceSum(DieRoll r1, DieRoll r2) {
+        super(0, 0, 0); // Gọi constructor của lớp cha DieRoll với các tham số giả định
+        this.r1 = r1;
+        this.r2 = r2;
+
+        logger.info("DiceSum instance created with two DieRoll objects.");
     }
 
+    // Ghi đè phương thức makeRoll() để trả về kết quả là sự kết hợp của 2 lần tung xúc xắc
+    @Override
+    public RollResult makeRoll() {
+        logger.info("Starting makeRoll() in DiceSum.");
+
+        RollResult result1 = r1.makeRoll(); // Gọi tung xúc xắc lần 1
+        logger.fine("First roll result: " + result1);
+
+        RollResult result2 = r2.makeRoll(); // Gọi tung xúc xắc lần 2
+        logger.fine("Second roll result: " + result2);
+
+        RollResult combined = result1.andThen(result2); // Kết hợp kết quả
+        logger.info("Combined roll result: " + combined);
+
+        return combined;
+    }
+
+    // Ghi đè phương thức toString() để hiển thị thông tin cả 2 lần tung xúc xắc
+    @Override
+    public String toString() {
+        return r1.toString() + " & " + r2.toString();
+    }
 }
